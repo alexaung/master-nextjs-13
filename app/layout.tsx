452153d93bf5e61a082b1698c9d29006fcb70813
aungmyooo <aungmo@gmail.com>
2023-06-15 20:56:10 +0630

    layout

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import { Inter } from "next/font/google";
+import { NavBar, Footer } from "./components";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Car Hub",
+  description: "Discover the best cars in the world.",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <NavBar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
