f05bb91caf07d30185ee556e6c0c737015f46c67
aungmyooo <aungmo@gmail.com>
2023-06-16 00:19:02 +0630

    Car Catalogue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
-import { NavBar, Footer } from "./components";
+import { NavBar, Footer } from "../components";
 
 const inter = Inter({ subsets: ["latin"] });
 
